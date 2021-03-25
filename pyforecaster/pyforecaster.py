df97ca7784991d8f1a73ac06a0866a274537733f
nepslor <nepslor@gmail.com>
2021-03-25 18:17:48 +0100

    first unittest

--- pyforecaster/pyforecaster.py
+++ pyforecaster/pyforecaster.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 
 def format_dataset(x):
+    assert isinstance(x, pd.DataFrame), 'x must be an instance of pandas.DataFrame'
     return x
---
+++ tests/formatter_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pyforecaster.pyforecaster import format_dataset
+
+
+class FormatDatasetTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.t = 100
+        self.n = 10
+        self.x = pd.DataFrame(np.random.randn(self.t, self.n))
+
+    def check_returned_type(self):
+        x_frmt = format_dataset(self.x)
+        assert isinstance(x_frmt, pd.DataFrame)
+
+
+if __name__ == '__main__':
+    unittest.main()
