21a97e9af35e0d930e80ce9ab09c912dd2ee3e92
nepslor <nepslor@gmail.com>
2021-04-02 23:33:29 +0200

    added transformers

--- pyforecaster/pyforecaster.py
+++ pyforecaster/pyforecaster.py
@@ -1,7 +1,89 @@
 import numpy as np
 import pandas as pd
+from itertools import product
 
 
-def format_dataset(x):
-    assert isinstance(x, pd.DataFrame), 'x must be an instance of pandas.DataFrame'
-    return x
+class Transformer:
+    """
+    Defines and applies transformations through rolling time windows and lags
+    """
+    def __init__(self, names, functions=None, agg_freq=None, lags=None, logger=None):
+        """
+        :param names: list of columns of the target dataset to which this transformer applies
+        :param functions:
+        :param agg_freq:
+        :param lags:
+        :param logger: auxiliary logger
+        """
+        self.names = names
+        self.functions = functions
+        self.agg_freq = agg_freq
+        self.lags = lags
+        self.logger = logger
+
+    def transform(self, x, augment=True):
+        """
+        Add transformations to the x pd.DataFrame, as specified by the Transformer's attributes
+
+        :param x: pd.DataFrame
+        :param augment: if True augments the original x DataFrame, otherwise returns just the transforms DataFrame
+        :return: transformed DataFrame
+        """
+        assert np.all(np.isin(self.names, x.columns)), "transformers names, {},  " \
+                                                       "must be in x.columns:{}".format(self.names, x.columns)
+
+        data = x.copy() if augment else pd.DataFrame(index=x.index)
+
+        for name in self.names:
+            if self.functions:
+                d = x[name].copy()
+                inferred_freq = d.index.inferred_freq
+                inferred_freq = inferred_freq if any([i.isdigit() for i in inferred_freq]) else '1' + inferred_freq
+                agg_freq = self.agg_freq if self.agg_freq else inferred_freq
+                min_periods = int(pd.Timedelta(agg_freq) / pd.Timedelta(inferred_freq))
+                d = d.rolling(agg_freq, min_periods=min_periods).agg(self.functions)
+                names = ['{}_{}{}'.format(name, agg_freq.upper(), p) for p in d.columns]
+                d.columns = names
+            else:
+                d = x[[name]].copy()
+
+            if self.lags:
+                names = ['{}_lag_{}'.format(p[1], p[0]) for p in product(self.lags, d.columns)]
+                d = pd.concat([d.shift(l) for l in self.lags], axis=1)
+                d.columns = names
+            if self.logger:
+                self.logger.info('Added {} to the dataframe'.format(d.columns.tolist()))
+            data = pd.concat([data, d], axis=1)
+        return data
+
+
+def format_dataset(x, target_transformer, *args, logger=None):
+    """
+    Apply several Transforsmers instances to the x pd.DataFrame to create the feature DataFrame, and target_transformer
+    to create the target DataFrame
+
+    :param x: pd.DataFrame
+    :param target_transformer: target's transformer. Usually just specified by lags
+    :param args: indefinite list of different transformers generating the features' DataFrame
+    :param logger: auxiliary logger
+    :return: feaures' and target's DataFrames
+    """
+    target = target_transformer.transform(x)
+    for t in args:
+        out = t.transform(x)
+        x = pd.concat([x, out], axis=1)
+
+    x = x.loc[~np.any(x.isna(), axis=1) & ~np.any(target.isna(), axis=1)]
+    target = target.loc[~np.any(x.isna(), axis=1) & ~np.any(target.isna(), axis=1)]
+    return x, target
+
+
+def tr_te_split(data: pd.DataFrame, split_ratio: float=0.75):
+    """
+    Divides data into training and test sets
+    :param data:
+    :param split_ratio:
+    :return:
+    """
+    n_tr = int(len(data)*split_ratio)
+    return data.iloc[:n_tr, :], data.iloc[n_tr:, :]
--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,18 +1,39 @@
 import unittest
 import pandas as pd
 import numpy as np
-from pyforecaster.pyforecaster import format_dataset
+import pyforecaster.pyforecaster as pyf
+import logging
 
 
 class TestFormatDataset(unittest.TestCase):
     def setUp(self) -> None:
-        self.t = 100
+        self.t = 25
         self.n = 10
-        self.x = pd.DataFrame(np.random.randn(self.t, self.n))
+        self.x = pd.DataFrame(np.random.randn(self.t, self.n), index=pd.date_range('01-01-2020', '01-02-2020', self.t))
+        self.logger =logging.getLogger()
+        logging.basicConfig(format='%(asctime)-15s::%(levelname)s::%(funcName)s::%(message)s', level=logging.INFO,
+                            filename=None)
 
-    def test_returned_type(self):
-        x_frmt = format_dataset(self.x)
-        assert isinstance(x_frmt, pd.DataFrame)
+    def test_transformer(self):
+
+        tr = pyf.Transformer([0], ['mean'], logger=self.logger)
+        _ = tr.transform(self.x)
+        tr = pyf.Transformer([0], ['mean'], agg_freq='2h', logger=self.logger)
+        _ = tr.transform(self.x)
+        tr = pyf.Transformer([0], ['mean'], agg_freq='2h', lags=[1, 2, -1, -2], logger=self.logger)
+        _ = tr.transform(self.x)
+        tr = pyf.Transformer([0, 1], ['mean', 'max'], '3h', [-1, -2], logger=self.logger)
+        _ = tr.transform(self.x)
+        tr = pyf.Transformer([0, 1], ['mean', 'max', lambda x: x**2-1], '3h', [-1, -2], logger=self.logger)
+        _ = tr.transform(self.x)
+        tr = pyf.Transformer([0], agg_freq='3h', lags=[-1, -2, -3], logger=self.logger)
+        _ = tr.transform(self.x, augment=False)
+
+        tr = pyf.Transformer([0], ['mean'], '3h', [-1], logger=self.logger)
+        x_tr = tr.transform(self.x, augment=False)
+
+        assert x_tr.shape[1] == 1
+
 
 
 if __name__ == '__main__':
