9a37ad595cdf8daaf1d70098edec76bb9ec43cce
nepslor <nepslor@gmail.com>
2021-10-06 17:41:54 +0200

    convert generator to list

--- pyforecaster/trainer.py
+++ pyforecaster/trainer.py
@@ -31,22 +31,23 @@ def objective(trial, x: pd.DataFrame, y: pd.DataFrame, model, cv, scoring=nmae, 
     # set optuna parameter space
     param_space = param_space_fun(trial)
     model.set_params(**param_space)
+    # create generator
+    cv_gen = (f for f in cv)
 
     # retrieve cross validation score
     if hpo_type == 'full':
-        cv_results = cross_validate(model, x, y, scoring=scoring, cv=cv, **cv_kwargs)
+        cv_results = cross_validate(model, x, y, scoring=scoring, cv=cv_gen, **cv_kwargs)
         scores = cv_results['test_score']
         trial.set_user_attr('cv_test_scores', scores)
         score = np.mean(scores)
     elif hpo_type == 'one_fold':
-        tr_idx, te_idx = cv[0]
+        tr_idx, te_idx = cv_gen[0]
         x_tr, x_te, y_tr, y_te = x.loc[tr_idx], x.loc[te_idx], y.loc[tr_idx], y.loc[te_idx]
         model.fit(x_tr, y_tr)
         preds = model.predict(x_te)
         score = scoring(preds, y_te)
     elif hpo_type == 'random_fold':
-        cv_folds = list(cv)
-        tr_idx, te_idx = np.random.choice(cv_folds, 1)
+        tr_idx, te_idx = np.random.choice(cv, 1)
         x_tr, x_te, y_tr, y_te = x.loc[tr_idx], x.loc[te_idx], y.loc[tr_idx], y.loc[te_idx]
         model.fit(x_tr, y_tr)
         preds = model.predict(x_te)
@@ -57,11 +58,13 @@ def objective(trial, x: pd.DataFrame, y: pd.DataFrame, model, cv, scoring=nmae, 
     return score
 
 
-def hyperpar_optimizer(x, y, model, n_trials, scoring, cv=5, param_space_fun=default_param_space_fun,
+def hyperpar_optimizer(x, y, model, n_trials, scoring, cv, param_space_fun=default_param_space_fun,
                        hpo_type='full', sampler=None, **cv_kwargs):
     if sampler is None:
         sampler = optuna.samplers.TPESampler()
     study = optuna.create_study(direction="minimize", sampler=sampler)
+    if cv is not list:
+        cv = list(cv)
     obj_fun = partial(objective, x=x, y=y, model=model, cv=cv, scoring=scoring, param_space_fun=param_space_fun,
                       hpo_type=hpo_type, **cv_kwargs)
     study.optimize(obj_fun, n_trials=n_trials)
