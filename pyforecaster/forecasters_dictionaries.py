21cf414d80d8024dee429195284143b9e84108b0
nepslor <nepslor@gmail.com>
2023-02-08 17:39:12 +0100

    added forecasters map

--- pyforecaster/forecasters_dictionaries.py
+++ pyforecaster/forecasters_dictionaries.py
@@ -1,5 +1,5 @@
-from . import forecaster
-from forecasting_models.holtwinters import HoltWinters, HoltWintersMulti
+from pyforecaster import forecaster
+from pyforecaster.forecasting_models.holtwinters import HoltWinters, HoltWintersMulti
 
 FORECASTER_MAP = {'Linear': forecaster.LinearForecaster,
                   'LGB': forecaster.LGBForecaster,
