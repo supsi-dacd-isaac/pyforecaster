162625ec3bb00f284f9ea4af03749f2331d66751
nepslor <nepslor@gmail.com>
2023-02-08 17:15:36 +0100

    added forecasters map

--- pyforecaster/dictionaries.py
+++ pyforecaster/dictionaries.py
@@ -1,5 +1,8 @@
 from . import copula
 from . import tree_builders
+from . import forecaster
+from forecasting_models.holtwinters import HoltWinters, HoltWintersMulti
+
 
 COPULA_MAP = {'HourlyGaussianCopula': copula.HourlyGaussianCopula,
               'ConditionalGaussianCopula': copula.ConditionalGaussianCopula}
@@ -8,3 +11,9 @@ TREE_MAP = {'DiffTree': tree_builders.DiffTree,
             'NeuralGas': tree_builders.NeuralGas,
             'ScenredTree': tree_builders.ScenredTree,
             'QuantileTree': tree_builders.QuantileTree}
+
+FORECASTER_MAP = {'Linear': forecaster.LinearForecaster,
+                  'LGB': forecaster.LGBForecaster,
+                  'HoltWinters': HoltWinters,
+                  'HoltWintersMulti': HoltWintersMulti
+                  }
