ebef36ec1e7bc1fcdeedf4ee8360bc452ce5c9e0
nepslor <nepslor@gmail.com>
2023-02-10 08:56:18 +0100

    added LGBHybrid to the forecaster map

--- pyforecaster/forecasters_dictionaries.py
+++ pyforecaster/forecasters_dictionaries.py
@@ -1,8 +1,10 @@
 from pyforecaster import forecaster
 from pyforecaster.forecasting_models.holtwinters import HoltWinters, HoltWintersMulti
+from pyforecaster.forecasting_models.gradientboosters import LGBMHybrid
 
 FORECASTER_MAP = {'Linear': forecaster.LinearForecaster,
                   'LGB': forecaster.LGBForecaster,
                   'HoltWinters': HoltWinters,
-                  'HoltWintersMulti': HoltWintersMulti
+                  'HoltWintersMulti': HoltWintersMulti,
+                  'LGBHybrid': LGBMHybrid
                   }
