implemented qicnn, causal loss function, corrected missing weights re-projection

tests/test_nns.py
@@ -1,5 +1,6 @@
 import unittest
 import matplotlib.pyplot as plt
+import optax
 import pandas as pd
 import numpy as np
 import logging
@@ -56,7 +57,7 @@ class TestFormatDataset(unittest.TestCase):
         x_tr, x_te, y_tr, y_te = [x.iloc[:n_tr, :].copy(), x.iloc[n_tr:, :].copy(), y.iloc[:n_tr].copy(),
                                   y.iloc[n_tr:].copy()]
 
-        savepath_tr_plots = 'tests/results/ffnn_tr_plots'
+        savepath_tr_plots = 'tests/results/figs/convexity'
 
         # if not there, create directory savepath_tr_plots
         if not exists(savepath_tr_plots):
@@ -70,11 +71,28 @@ class TestFormatDataset(unittest.TestCase):
                   n_out=y_tr.shape[1], n_layers=3, optimization_vars=optimization_vars).fit(x_tr,
                                                                                             y_tr,
                                                                                             n_epochs=1,
-                                                                                            savepath_tr_plots=savepath_tr_plots,
                                                                                             stats_step=40)
 
         y_hat_1 = m_1.predict(x_te)
         m_1.save('tests/results/ffnn_model.pk')
+
+        rnd_idxs = np.random.choice(x_tr.shape[0], 1)
+        rand_opt_vars = np.random.choice(optimization_vars, 10)
+        for cc in rand_opt_vars:
+            x = x_tr.iloc[rnd_idxs, :]
+            x = pd.concat([x] * 100, axis=0)
+            x[cc] = np.linspace(-1, 1, 100)
+            y_hat = m_1.predict(x)
+            d = np.diff(np.sign(np.diff(y_hat.values[:, :96], axis=0)), axis=0)
+            approx_second_der = np.round(np.diff(y_hat.values[:, :96], 2, axis=0), 5)
+            approx_second_der[approx_second_der == 0] = 0  # to fix the sign
+            is_convex = not np.any(np.abs(np.diff(np.sign(approx_second_der), axis=0)) > 1)
+            print('output is convex w.r.t. input {}: {}'.format(cc, is_convex))
+            plt.figure(layout='tight')
+            plt.plot(np.tile(x[cc].values.reshape(-1, 1), 96), y_hat.values[:, :96], alpha=0.3)
+            plt.xlabel(cc)
+            plt.show()
+            plt.savefig('wp3/results/figs/convexity/picnn_{}.png'.format(cc), dpi=300)
 
         n = PICNN(load_path='tests/results/ffnn_model.pk')
         y_hat_2 = n.predict(x_te.iloc[:100, :])
@@ -91,10 +109,10 @@ class TestFormatDataset(unittest.TestCase):
         assert np.all(np.mean((y_hat_1-y_hat_2).abs()) <= 1e-6)
 
 
-    def no_test_pqicnn(self):
+    def test_causal_df(self):
         # normalize inputs
-        x = (self.x - self.x.mean(axis=0)) / (self.x.std(axis=0)+0.01)
-        y = (self.y - self.y.mean(axis=0)) / (self.y.std(axis=0)+0.01)
+        x = (self.x - self.x.mean(axis=0)) / (self.x.std(axis=0) + 0.01)
+        y = (self.y - self.y.mean(axis=0)) / (self.y.std(axis=0) + 0.01)
 
         n_tr = int(len(x) * 0.8)
         x_tr, x_te, y_tr, y_te = [x.iloc[:n_tr, :].copy(), x.iloc[n_tr:, :].copy(), y.iloc[:n_tr].copy(),
@@ -106,19 +124,43 @@ class TestFormatDataset(unittest.TestCase):
         if not exists(savepath_tr_plots):
             makedirs(savepath_tr_plots)
 
+        optimization_vars = x_tr.columns[:2]
+        causal_df = pd.DataFrame(np.tril(np.ones((len(optimization_vars), y_tr.shape[1]))), index=optimization_vars, columns=y_tr.columns)
 
-        optimization_vars = x_tr.columns[:-1]
+        m_1 = PICNN(learning_rate=1e-3, batch_size=1000, load_path=None, n_hidden_x=200, n_hidden_y=200,
+                    n_out=y_tr.shape[1], n_layers=3, optimization_vars=optimization_vars, causal_df = causal_df).fit(x_tr,
+                                                                                              y_tr,
+                                                                                              n_epochs=1,
+                                                                                              savepath_tr_plots=savepath_tr_plots,
+                                                                                              stats_step=10)
+    def test_pqicnn(self):
+
+        # normalize inputs
+        x = (self.x - self.x.mean(axis=0)) / (self.x.std(axis=0)+0.01)
+        y = (self.y - self.y.mean(axis=0)) / (self.y.std(axis=0)+0.01)
+
+        n_tr = int(len(x) * 0.8)
+        x_tr, x_te, y_tr, y_te = [x.iloc[:n_tr, :].copy(), x.iloc[n_tr:, :].copy(), y.iloc[:n_tr].copy(),
+                                  y.iloc[n_tr:].copy()]
+
+        savepath_tr_plots = 'tests/results/figs/convexity'
+
+        # if not there, create directory savepath_tr_plots
+        if not exists(savepath_tr_plots):
+            makedirs(savepath_tr_plots)
 
 
-        m_1 = PQICNN(learning_rate=1e-2, batch_size=1000, load_path=None, n_hidden_x=200, n_hidden_y=200,
-                  n_out=y_tr.shape[1], n_layers=4, optimization_vars=optimization_vars,stopping_rounds=100).fit(x_tr,
+        optimization_vars = x_tr.columns[:10]
+        optimizer = optax.adamw(learning_rate=1e-3)
+
+        m_1 = PQICNN(learning_rate=1e-3, batch_size=1000, load_path=None, n_hidden_x=200, n_hidden_y=200,
+                  n_out=y_tr.shape[1], n_layers=4, optimization_vars=optimization_vars,stopping_rounds=100, optimizer=optimizer).fit(x_tr,
                                                                                             y_tr,
-                                                                                            n_epochs=30,
+                                                                                            n_epochs=1,
                                                                                             savepath_tr_plots=savepath_tr_plots,
-                                                                                            stats_step=100,rel_tol=-1)
-        y_hat_1 = m_1.predict(x_te)
+                                                                                            stats_step=80,rel_tol=-1)
 
-         # check convexity of the PICNN
+        # check convexity of the PICNN
         rnd_idxs = np.random.choice(x_tr.shape[0], 1)
         rand_opt_vars = np.random.choice(optimization_vars, 10)
         for cc in rand_opt_vars: