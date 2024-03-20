a60db39a437aa715311bae243f4b49fdbfd2a79b
nepslor <nepslor@gmail.com>
2024-03-20 22:01:12 +0100

    khw

--- tests/holt_winters.py
+++ tests/holt_winters.py
@@ -3,6 +3,7 @@ import numpy as np
 import wget
 import matplotlib.pyplot as plt
 from matplotlib import animation
+from tqdm import tqdm
 
 data = pd.read_pickle('tests/data/test_data.zip')['y'][['all']]
 data /= data.std()
@@ -30,7 +31,7 @@ def ts_animation(y_te, y_hat, n_rows=50, labels=None):
     line2.set_data([], [])
     return (line1,line2,)
 
-  ani = animation.FuncAnimation(fig, animate, init_func=init, frames=n_rows, interval=10,
+  ani = animation.FuncAnimation(fig, animate, init_func=init, frames=n_rows, interval=1,
                                 blit=True)
   return ani
 
@@ -72,7 +73,7 @@ y_hat = np.vstack(y_hat)
 seasonal_state = np.vstack(seasonal_state)
 
 
-ts_animation(data['all'].values, y_hat, 300, labels=['ground truth', 'predictions'])
+#ts_animation(data['all'].values, y_hat, 300, labels=['ground truth', 'predictions'])
 
 
 # Fourier Holt-Winters: in this Holt-Winters forecasters the states are the Fourier coefficients
@@ -87,7 +88,116 @@ def get_basis(t, l, n_h):
   return P
 
 
-def fourier_hw(y, h=1, alpha=0.8, m=24, omega=0.99, n_harmonics=3, return_coeffs=False):
+class fourier_hw:
+
+
+    def __init__(self, h=1, alpha=0.8, m=24, omega=0.99, n_harmonics=3):
+        """
+        :param y:
+        :param h:
+        :param alpha:
+        :param m:
+        :return:
+        """
+        assert m>0, 'm must be positive'
+        assert 0<alpha<1, 'alpha must be in (0, 1)'
+        assert 0<omega<1, 'omega must be in (0, 1)'
+        assert n_harmonics>0, 'n_harmonics must be positive'
+
+        # precompute basis over all possible periods
+        self.alpha = alpha
+        self.omega = omega
+        self.h=h
+        self.m = m
+        Ps_past = []
+        Ps_future = []
+        for i in range(m):
+            t = np.arange(m)+i
+            t_f = np.arange(m+h)+i
+            self.n_harmonics = np.minimum(n_harmonics, m // 2)
+            P = get_basis(t, m, self.n_harmonics)
+            Ps_past.append(P)
+            P_f = get_basis(t_f, m, self.n_harmonics)
+            P_f = P_f[m:, :]
+            Ps_future.append(P_f)
+        self.Ps_future = Ps_future
+        self.Ps_past = Ps_past
+        self.coeffs = None
+        self.eps = None
+    def predict(self, y, return_coeffs=False, start_from=0):
+        if self.coeffs is None:
+            coeffs = np.zeros(2 * self.n_harmonics + 1)
+            coeffs[0] = y[0]*np.sqrt(m)
+            eps = 0
+            preds = [[0]]
+        else:
+            eps = self.eps
+            coeffs = self.coeffs
+            preds = [[y[start_from]+eps]]
+
+        coeffs_t_history = []
+        for i in range(1 + start_from, len(y)+1):
+            P = self.Ps_past[(i)%self.m]
+            P_f = self.Ps_future[(i)%self.m]
+            start = np.maximum(0, i-self.m)
+            w = y[start:i].copy()
+            eps = self.omega * (w[-1] - preds[-1][0]) + (1-self.omega) * eps
+            w[-1] = w[-1] -eps
+            coeffs_t = P[-len(w):,:].T@w
+            coeffs = self.alpha*coeffs_t + (1-self.alpha)*coeffs
+
+            preds.append((P_f@coeffs).ravel() + eps*(self.h-np.arange(self.h))**2/self.h**2)
+            if return_coeffs:
+                coeffs_t_history.append(coeffs)
+        self.coeffs = coeffs
+        self.eps = eps
+        if return_coeffs:
+            return np.vstack(preds[1:]), np.vstack(coeffs_t_history)
+        return np.vstack(preds[1:])
+
+    def __getstate__(self):
+        return (self.coeffs, self.eps)
+    def __setstate__(self, state):
+        self.coeffs, self.eps = state
+
+from pyforecaster.forecaster import LinearForecaster
+from pyforecaster.formatter import Formatter
+fr = Formatter().add_transform(['all'], ['mean'], lags=np.arange(1, 144))
+fr.add_target_transform(['all'], ['mean'], lags=-np.arange(1, 145))
+x, y = fr.transform(data, time_features=False)
+
+n_tr = 10000
+n_te = 10000
+
+lf = LinearForecaster().fit(x.iloc[:n_tr,:], y.iloc[:n_tr,:])
+
+y_hat_lin = lf.predict(x.iloc[n_tr:n_tr+n_te,:])
+ts_animation(y.iloc[n_tr:n_tr+n_te,0].values, y_hat_lin.values, 2000, labels=['ground truth', 'predictions'])
+
+
+m = 24*6
+
+
+y_hat, coeffs_t = fourier_hw(h=24*6, alpha=0.9, omega=0.9, n_harmonics=20, m=m).predict(data['all'].values[:2100], return_coeffs=True)
+"""
+ts_animation(x.iloc[:, 0].values, y_hat, 2000, labels=['ground truth', 'predictions'])
+ts_animation(x.iloc[:, 0].values, coeffs_t[:, :], 2000, labels=['ground truth', 'coeffs'])
+
+
+mae = lambda x, y: np.mean(np.abs(x-y))
+mae(y.iloc[n_tr:n_tr+n_te,:].values, y_hat_lin.values)
+mae(y.iloc[n_tr:n_tr+n_te,:].values, y_hat[n_tr:n_tr+n_te, :])
+
+"""
+
+# Fourier-Kalman exponential smoothing
+
+
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+
+
+def fourier_kexp(y, h=1, alpha=0.8, m=24, omega=0.99, n_harmonics=3, return_coeffs=False):
     """
     :param y:
     :param h:
@@ -95,22 +205,36 @@ def fourier_hw(y, h=1, alpha=0.8, m=24, omega=0.99, n_harmonics=3, return_coeffs
     :param m:
     :return:
     """
+
+    n_harmonics = np.minimum(n_harmonics, m // 2)
+
+    coeffs = np.zeros(2*n_harmonics+1)
+    preds = [[0]]
+    coeffs_t_history = []
+    coeffs[0] = y[0]*np.sqrt(m)
+
     # precompute basis over all possible periods
+    my_filter = KalmanFilter(dim_x=n_harmonics*2+1, dim_z=n_harmonics*2+1)
+    my_filter.x = coeffs.copy()
+
+    my_filter.F = np.eye(n_harmonics*2+1)
+
+    my_filter.H = np.eye(n_harmonics*2+1)
+    my_filter.P = np.eye(n_harmonics*2+1) * 1000
+    my_filter.R = np.eye(n_harmonics*2+1) * 0.01
+    my_filter.Q = 0.01
+
     Ps_past = []
     Ps_future = []
     for i in range(m):
         t = np.arange(m)+i
         t_f = np.arange(m+h)+i
-        n_harmonics = np.minimum(n_harmonics, m // 2)
         P = get_basis(t, m, n_harmonics)
         Ps_past.append(P)
         P_f = get_basis(t_f, m, n_harmonics)
         P_f = P_f[m:, :]
         Ps_future.append(P_f)
-    coeffs = np.zeros(2*n_harmonics+1)
-    preds = [[0]]
-    coeffs_t_history = []
-    coeffs[0] = y[0]*np.sqrt(m)
+
     eps = 0
     for i in range(1, len(y)-1):
         P = Ps_past[i%m]
@@ -120,18 +244,116 @@ def fourier_hw(y, h=1, alpha=0.8, m=24, omega=0.99, n_harmonics=3, return_coeffs
         eps = omega * (w[-1] - preds[-1][0]) + (1-omega) * eps
         w[-1] = w[-1] -eps
         coeffs_t = P[-len(w):,:].T@w
-        coeffs = alpha*coeffs_t + (1-alpha)*coeffs
+
+        my_filter.predict()
+        my_filter.update(coeffs_t)
+        coeffs = my_filter.x
 
         preds.append((P_f@coeffs).ravel() + eps*(h-np.arange(h))**2/h**2)
         if return_coeffs:
-            coeffs_t_history.append(coeffs)
+            coeffs_t_history.append(coeffs_t)
+        if i%m==0 :
+            my_filter.Q = np.corrcoef(np.vstack(coeffs_t_history).T)
+            my_filter.R = np.corrcoef(np.vstack(coeffs_t_history).T)*0.01
+            my_filter.P = np.eye(n_harmonics*2+1) * 1000
+
+
+    if return_coeffs:
+        return np.vstack(preds[1:]), np.vstack(coeffs_t_history)
+    return np.vstack(preds[1:])
+
+m = 24*6
+data_te = x.iloc[:, 0].values[:2100].copy()
+data_te[500:] -= 5
+data_te[1000:] += 5
+y_hat_kexp, coeffs_t = fourier_kexp(data_te, h=24*6, alpha=0.9, omega=0.9, n_harmonics=20, m=m, return_coeffs=True)
+ts_animation(data_te, y_hat_kexp, 2000, labels=['ground truth', 'predictions'])
+ts_animation(data_te, coeffs_t, 2000, labels=['ground truth', 'predictions'])
+
+
+
+def fourier_kexp_2(y, h=1, m=24, omega=0.99, n_harmonics=3, return_coeffs=False):
+    """
+    :param y:
+    :param h:
+    :param alpha:
+    :param m:
+    :return:
+    """
+
+    n_predictors = 4
+    n_harmonics = np.minimum(n_harmonics, m // 2)
+
+    coeffs = np.zeros(2*n_harmonics+1)
+    preds = [[0]]
+    coeffs_t_history = []
+    coeffs[0] = y[0]*np.sqrt(m)
+
+    # precompute basis over all possible periods
+    my_filter = KalmanFilter(dim_x=n_predictors, dim_z=n_predictors)
+    my_filter.x = np.ones(n_predictors) / n_predictors
+
+    my_filter.F = np.eye(n_predictors)
+    my_filter.H = np.eye(n_predictors)
+    my_filter.P = np.eye(n_predictors) * 1000
+    my_filter.R = np.eye(n_predictors)
+    my_filter.Q = 0.1
+
+    models = [fourier_hw(h=h, alpha=0.9, omega=0.9, n_harmonics=n_harmonics, m=h*(i+1)) for i in range(n_predictors)]
+    states = [models[j].__getstate__() for j in range(n_predictors)]
+
+
+    preds_models = [[] for i in range(n_predictors)]
+    for i in tqdm(np.arange(len(y))):
+
+        [models[j].__setstate__(states[j]) for j in range(n_predictors)]
+        preds_t = [models[j].predict(y[:i+1].copy(), return_coeffs=False, start_from=i).ravel() for j in range(n_predictors)]
+        for j in range(n_predictors):
+            try:
+                preds_models[j].append(preds_t[j])
+            except:
+                print(3)
+        if i>=h:
+            for j in range(n_predictors):
+                preds_models[j].pop(0)
+        # averate last point error over different prediction times for all the models
+            avg_err = [np.mean([np.abs(p[-(k+1)]-y[i]) for k, p in enumerate(preds_models[j])]) for j in range(n_predictors)]
+            coeffs_t = np.exp(-np.array(avg_err)) / np.exp(-np.array(avg_err)).sum()
+        else:
+            coeffs_t = np.ones(n_predictors) / n_predictors
+        states = [models[j].__getstate__() for j in range(n_predictors)]
+
+        my_filter.predict()
+        my_filter.update(coeffs_t)
+        coeffs = my_filter.x
+        coeffs = coeffs / np.abs(coeffs).sum()
+        preds.append(np.vstack(preds_t).T@coeffs)
+
+        if return_coeffs:
+            coeffs_t_history.append(coeffs_t)
+        if i%m==0 and i>=m:
+            my_filter.Q = np.corrcoef(np.vstack(coeffs_t_history).T)
+            my_filter.R = np.corrcoef(np.vstack(coeffs_t_history).T)*10
+
     if return_coeffs:
         return np.vstack(preds[1:]), np.vstack(coeffs_t_history)
     return np.vstack(preds[1:])
 
 
-m = 24*6*3
-y_hat, coeffs_t = fourier_hw(data['all'].iloc[:2100].copy().values, h=24*6, alpha=0.9, omega=0.5, n_harmonics=20, m=m, return_coeffs=True)
 
-ts_animation(data['all'].values, y_hat, 2000, labels=['ground truth', 'predictions'])
-ts_animation(data['all'].values, coeffs_t[:, :], 2000, labels=['ground truth', 'coeffs'])
+data.iloc[11500:12500]-=5
+x, y = fr.transform(data, time_features=False)
+
+n_tr = 10000
+n_te = 2800
+
+m = 24*6*7
+y_hat_kexp, coeffs_t = fourier_kexp_2(x.iloc[n_tr:n_tr+n_te,0].values, h=24*6, omega=0.9, n_harmonics=20, m=m, return_coeffs=True)
+ts_animation(x.iloc[n_tr:n_tr+n_te,0].values, y_hat_kexp, 2000, labels=['ground truth', 'predictions'])
+ts_animation(x.iloc[n_tr:n_tr+n_te,0].values, coeffs_t, 2000, labels=['ground truth', 'predictions'])
+
+
+lf = LinearForecaster().fit(x.iloc[:n_tr,:], y.iloc[:n_tr,:])
+
+y_hat_lin = lf.predict(x.iloc[n_tr:n_tr+n_te,:])
+ts_animation(x.iloc[n_tr:n_tr+n_te,0].values, y_hat_lin.values, 2000, labels=['ground truth', 'predictions'])
